b1580008300a4b54dff38be2ce87fe869e1aa743
Willow Hayward <willow.hayward@autogeneral.com.au>
2024-12-01 16:52:13 +1000

    Star 2 complete

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,9 +1,11 @@
+use std::collections::HashMap;
+
 pub fn run(challenge: &String, input: String) -> String {
     match challenge.as_str() {
         "silver" => run_silver(input),
         "gold" => run_gold(input),
-        _ => panic!("Challenge must be silver or gold") // TODO: This could probably be generalised
-        // for a future day
+        _ => panic!("Challenge must be silver or gold"), // TODO: This could probably be generalised
+                                                         // for a future day
     }
 }
 
@@ -33,7 +35,10 @@ fn parse_lists(input: String) -> (Vec<i32>, Vec<i32>) {
 }
 
 fn parse_line(line: &str) -> (i32, i32) {
-    let chunks: Vec<i32> = line.split_whitespace().map(|chunk| chunk.parse().unwrap()).collect();
+    let chunks: Vec<i32> = line
+        .split_whitespace()
+        .map(|chunk| chunk.parse().unwrap())
+        .collect();
 
     (chunks[0], chunks[1])
 }
@@ -55,7 +60,45 @@ fn compare_lists(left_list: Vec<i32>, right_list: Vec<i32>) -> i32 {
 }
 
 fn run_gold(input: String) -> String {
-    todo!()
+    let lists = parse_lists(input);
+
+    let left_duplicates = count_duplicates(lists.0);
+    let right_duplicates = count_duplicates(lists.1);
+
+    let score = calculate_similarity_score(left_duplicates, right_duplicates);
+
+    score.to_string()
+}
+
+fn count_duplicates(list: Vec<i32>) -> HashMap<i32, i32> {
+    let mut duplicates: HashMap<i32, i32> = HashMap::new();
+
+    for item in list {
+        let current = duplicates.get(&item);
+        let total = match current {
+            Some(i) => i + 1,
+            None => 1,
+        };
+        duplicates.insert(item, total);
+    }
+
+    duplicates
+}
+
+fn calculate_similarity_score(left_duplicates: HashMap<i32, i32>, right_duplicates: HashMap<i32, i32>) -> i32 {
+    let mut score = 0;
+
+    for i in left_duplicates.keys() {
+        let left_value = left_duplicates.get(&i).unwrap();
+        let right_value: i32 = match right_duplicates.get(&i) {
+            Some(v) => *v,
+            None => 0
+        };
+
+        score += i * left_value * right_value;
+    }
+
+    score
 }
 
 #[cfg(test)]
@@ -75,9 +118,22 @@ mod tests {
         let input_1 = "\
 1    8
 3    1
-2    4".to_string();
+2    4"
+            .to_string();
         let test_1 = parse_lists(input_1);
         assert_eq!(test_1.0, vec![1, 2, 3]);
         assert_eq!(test_1.1, vec![1, 4, 8]);
+    }
+
+    #[test]
+    fn test_count_duplicates() {
+        let input_1 = vec![1, 2, 3, 3, 3, 1];
+        let test_1 = count_duplicates(input_1);
+        let expected_1 = HashMap::from([
+            (1, 2),
+            (2, 1),
+            (3, 3)
+        ]);
+        assert_eq!(test_1, expected_1);
     }
 }
